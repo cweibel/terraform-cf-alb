82351b2930d5449817ead825d051069038a70dfd
cweibel <christopher.weibel@gmail.com>
2022-05-17 16:22:51 -0400

    First commit

---
+++ variables.tf
@@ -0,0 +1,3 @@
+variable "env_id"         { default = "dev2"}
+variable "aws_region"     { default = "us-west-2" } # AWS Region
+variable "aws_vpc_id"     {} # ID of your VPC                           (required)
